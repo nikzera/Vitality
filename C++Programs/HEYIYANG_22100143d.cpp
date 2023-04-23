2aef9cf36b2bff709aaae448f538d41bcee4dda7
nikzera <131600163+nikzera@users.noreply.github.com>
2023-04-23 22:50:52 +0800

    Add files via upload

---
+++ C++Programs/HEYIYANG_22100143d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int List1[1000];
+    int n = 0;
+    int sum = 0;
+    int x{};
+    while (x != -999) {
+        std::cin >> x;
+        if (x != -999) {
+            List1[n] = x;
+            sum = sum + List1[n] * (int)std::pow(-1, n);
+            n++;
+        }
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
